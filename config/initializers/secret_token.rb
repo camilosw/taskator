4fca784b33e5c11774d1182b79b34a8b0ad68b60
camilosw <camilosw@gmail.com>
2012-09-30 15:45:02 -0500

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Taskator::Application.config.secret_token = 'bd0089d18b25a6112a05e49e3d1bcd411f81209b427025a9de6b4faf5d0b206b2032668090f5655fa551a8228a449ed34efb87ba03ba2e8a110fd7b4718c9f46'
